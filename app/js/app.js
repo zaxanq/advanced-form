c159437d95c6e9166c320bd4b4083031f0e64b2d
zaxanq <zaxanq@gmail.com>
2019-05-30 14:30:45 +0200

    Initial app built and step implemented.

---
+++ app/js/app.js
@@ -0,0 +1,72 @@
+class Form {
+  init() {
+    this.step = 1;
+    this.stepCount = 5;
+    this.registerElements();
+    this.setStep();
+  }
+
+  registerElements() {
+    this.tabs = this.class('tab', false);
+    this.pages = this.class('content__page', false);
+    this.setTabClicks();
+  }
+
+  setTabClicks() {
+    for (let i = 0; i < this.tabs.length; i++) {
+      this.tabs[i].addEventListener('click', () => {
+        if (this.allowStepChange(i)) {
+          this.changeStep(i + 1);
+        }
+      });
+    }
+  }
+
+  allowStepChange(i) {
+    if (this.hasClass('tab--active', this.tabs[i])) return false;
+    if (this.hasClass('tab--disabled', this.tabs[i])) return false;
+    return true;
+  }
+
+  setStep() {
+    for (let i = 0; i < this.stepCount; i++) {
+      this.removeClass('tab--active', this.tabs[i]);
+      this.removeClass('page--active', this.pages[i]);
+      this.removeClass('tab--disabled', this.tabs[i]);
+    }
+    for (let i = this.step + 1; i < this.stepCount; i++) {
+      if (!this.hasClass('tab--available', this.tabs[i])) {
+        this.addClass('tab--disabled', this.tabs[i]);
+      }
+    }
+    if (this.step < this.stepCount) {
+      this.addClass('tab--available', this.tabs[this.step]);
+    }
+    this.addClass('tab--active', this.tabs[this.step - 1]);
+    this.addClass('page--active', this.pages[this.step -1]);
+  }
+
+  changeStep(step) {
+    this.step = step;
+    this.setStep();
+  }
+
+  class(className, firstOnly = true) {
+    let elements = document.getElementsByClassName(className);
+    return firstOnly ? elements[0] : elements;
+  }
+
+  addClass(className, element) {
+    element.classList.add(className);
+  }
+
+  removeClass(className, element) {
+    element.classList.remove(className);
+  }
+
+  hasClass(className, element) {
+    return element.classList.contains(className);
+  }
+}
+
+(new Form).init();
