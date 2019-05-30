aa2599be3b3f08703cc6107e2fb7d02b510a879b
zaxanq <zaxanq@gmail.com>
2019-05-31 00:07:35 +0200

    Added regexp and first step validation.

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,16 @@ class Form {
     this.step = 1;
     this.stepCount = 5;
     this.registerElements();
+    this.disableStepChange();
     this.setStep();
     this.initValidators();
+    this.Data = {};
   }
 
   registerElements() {
     this.tabs = this.class('tab', false);
     this.pages = this.class('content__page', false);
+    this.nextStepButtons = this.class('button--next-step', false);
     this.setTabClicks();
   }
 
@@ -17,14 +20,14 @@ class Form {
   setTabClicks() {
     for (let i = 0; i < this.tabs.length; i++) {
       this.tabs[i].addEventListener('click', () => {
-        if (this.allowStepChange(i)) {
+        if (this.allowTabStepChange(i)) {
           this.changeStep(i + 1);
         }
       });
     }
   }
 
-  allowStepChange(i) {
+  allowTabStepChange(i) {
     if (this.hasClass('tab--active', this.tabs[i])) return false;
     if (this.hasClass('tab--disabled', this.tabs[i])) return false;
     return true;
@@ -36,7 +39,7 @@ class Form {
       this.removeClass('page--active', this.pages[i]);
       this.removeClass('tab--disabled', this.tabs[i]);
     }
-    for (let i = this.step + 1; i < this.stepCount; i++) {
+    for (let i = this.step; i < this.stepCount; i++) {
       if (!this.hasClass('tab--available', this.tabs[i])) {
         this.addClass('tab--disabled', this.tabs[i]);
       }
@@ -45,7 +48,7 @@ class Form {
       this.addClass('tab--available', this.tabs[this.step]);
     }
     this.addClass('tab--active', this.tabs[this.step - 1]);
-    this.addClass('page--active', this.pages[this.step -1]);
+    this.addClass('page--active', this.pages[this.step - 1]);
   }
 
   changeStep(step) {
@@ -57,30 +60,116 @@ class Form {
 
   initValidators() {
     this.textInputs = this.class('input--text', false);
-
     [...this.textInputs].map(input => {
-      let validator = input.nextElementSibling;
+      let validator = input.parentElement.previousElementSibling;
+      let errorContainer = input.nextElementSibling;
       input.addEventListener('input', () => {
-        if (this.validateInputs(input)) {
-          this.addClass('validation--invalid', validator);
-          this.removeClass('validation--valid', validator);
-        } else {
+        let validation = this.validateInput(input);
+        if (validation.result) {
           this.removeClass('validation--invalid', validator);
           this.addClass('validation--valid', validator);
+          this.renderInputError(errorContainer, '');
+          if (this.checkStepInputs(this.step)) {
+            this.allowStepChange();
+          } else {
+            // this.disableStepChange();
+            // Commented to style "Proceed" button easier.
+            // TODO: style "Proceed" button
+          }
+        } else {
+          this.addClass('validation--invalid', validator);
+          this.removeClass('validation--valid', validator);
+          this.renderInputError(errorContainer, validation.reason);
         }
+      });
+      input.addEventListener('focusout', () => {
+        this.saveInputValue(input);
       });
     })
   }
 
-  validateInputs(input) {
-    if (!input.checkValidity()) return false;
+  saveInputValue(input) {
+    this.Data[input.id] = input.value;
+    console.log(this.Data);
+  }
+
+  validateInput(input) {
+    if (input.id === 'username') {
+      if (!input.checkValidity()) {
+        return {result: false, reason: 'Username is too short.'}
+      }
+      if (!(new RegExp(/^[a-zA-Z0-9]/)).test(input.value)) {
+        return {result: false, reason: 'Username must start with a letter or a digit'};
+      }
+      if (!(new RegExp(/[a-zA-Z0-9]$/)).test(input.value)) {
+        return {result: false, reason: 'Username must end with a letter or a digit'};
+      }
+    } else if (input.id === 'email') {
+      if (!input.checkValidity()) {
+        return {result: false, reason: 'Email address incorrect.'}
+      }
+      if (!(new RegExp(/^[a-zA-Z0-9]/)).test(input.value)) {
+        return {result: false, reason: 'Email address must start with a letter or a digit'};
+      }
+    } else if (input.id === 'password') {
+      if (!input.checkValidity()) {
+        return {result: false, reason: 'Password is too short.'}
+      }
+      if ((new RegExp(/(\w)\1{2}/)).test(input.value)) {
+        return {result: false, reason: 'Password cannot have 3 same characters in a row.'}
+      }
+    } else if (input.id === 'repeat-password') {
+      if (input.value !== this.Data.password) {
+        return {result: false, reason: 'Passwords cannot be different.'}
+      }
+    }
+    return {result: true, reason: ''};
+  }
+
+  renderInputError(container, reason) {
+    container.innerText = reason;
+  }
+
+  checkStepInputs(step) {
+    if (step === 1) {
+      if (this.checkInputs(['username', 'email', 'password', 'repeat-password'])) {
+        return true;
+      }
+    } else if (step === 2) {
+      return true;
+    } else if (step === 3) {
+      return true;
+    } else if (step === 4) {
+      return true;
+    }
+    return false;
+  }
+
+  checkInputs(inputs) {
+    for (let i = 0; i < inputs.length; i++) {
+      if (!this.id(inputs[i]).value || this.id(inputs[i]).value === '' || !this.validateInput(this.id(inputs[i]))) {
+        return false;
+      }
+    }
     return true;
+  }
+
+  allowStepChange() {
+    this.nextStepButtons[this.step - 1].disabled = false;
+  }
+
+  disableStepChange() {
+    this.nextStepButtons[this.step - 1].disabled = true;
   }
 
   // -------------------- Helpful methods -------------------- //
   class(className, firstOnly = true) {
     let elements = document.getElementsByClassName(className);
     return firstOnly ? elements[0] : elements;
+  }
+
+  id(idName) {
+    return document.getElementById(idName);
   }
 
   addClass(className, element) {
